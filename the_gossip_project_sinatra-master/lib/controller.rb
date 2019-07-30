49e584fa62099d20dbed2984cf9f52ab175be67d
Syle <sylvain.firer@gmail.com>
2019-07-30 08:35:48 +0200

    last session 2

--- the_gossip_project_sinatra-master/lib/controller.rb
+++ the_gossip_project_sinatra-master/lib/controller.rb
@@ -15,13 +15,6 @@ class ApplicationController < Sinatra::Base
   redirect '/'
   end
 
-  # post '/gossips/new/' do
-  # puts "Salut, je suis dans le serveur"
-  # puts "Ceci est le contenu du hash params : #{params}"
-  # puts "Trop bien ! Et ceci est ce que l'utilisateur a passé dans le champ gossip_author : #{params["gossip_author"]}"
-  # puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
-  # puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
-  # end
 
   get '/all_gossips/' do
     erb :all_gossips, locals: {gossips: Gossip.all}
